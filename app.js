fe68f95d12f7a1e076824595a03c6fe9abdb898e
Dmitriy Kharlashin <dmitriy.kharlashin@just-eat.com>
2019-11-11 17:56:35 +0200

    Use DB singleton in items controller

--- app.js
+++ app.js
@@ -19,44 +19,45 @@ app.use(morgan('dev'));
 
 initRoutes(router);
 
-const db = new DB();
-db.getItems()
-  .then(items => {
-    console.log(items);
+app.db = new DB();
+// const db = new DB();
+// db.getItems()
+//   .then(items => {
+//     console.log(items);
 
-    return db.createItem({ price: 20, name: 'Hooba Booba' });
-  })
-  .then(item => {
-    console.log(item);
+//     return db.createItem({ price: 20, name: 'Hooba Booba' });
+//   })
+//   .then(item => {
+//     console.log(item);
 
-    return db.getItem(item.id);
-  })
-  .then(item => {
-    console.log(item);
+//     return db.getItem(item.id);
+//   })
+//   .then(item => {
+//     console.log(item);
 
-    return db.updateItem(item.id, {
-      price: 50,
-      name: 'Hooba Booba Mega DRIVE!!!',
-    });
-  })
-  .then(updatedItem => {
-    console.log(updatedItem);
-  })
-  .then(() => {
-    return db.getItems();
-  })
-  .then(items => {
-    console.log(items);
+//     return db.updateItem(item.id, {
+//       price: 50,
+//       name: 'Hooba Booba Mega DRIVE!!!',
+//     });
+//   })
+//   .then(updatedItem => {
+//     console.log(updatedItem);
+//   })
+//   .then(() => {
+//     return db.getItems();
+//   })
+//   .then(items => {
+//     console.log(items);
 
-    return db.deleteItem(items[items.length - 1].id);
-  })
-  .then(() => {
-    return db.getItems();
-  })
-  .then(items => {
-    console.log(items);
-  })
-  .catch(error => console.error(error));
+//     return db.deleteItem(items[items.length - 1].id);
+//   })
+//   .then(() => {
+//     return db.getItems();
+//   })
+//   .then(items => {
+//     console.log(items);
+//   })
+//   .catch(error => console.error(error));
 
 app
   .use(bodyParser())
--- src/controllers/items.js
+++ src/controllers/items.js
@@ -1,48 +1,69 @@
 const LIVR = require('livr');
 
-const db = require('../initializers/db');
-
 LIVR.Validator.defaultAutoTrim(true);
+const dataValidation = itemData => {
+  const validator = new LIVR.Validator({
+    price: 'positive_integer',
+    name: ['required', { max_length: 255 }],
+  });
+
+  return validator.validate(itemData);
+};
 
 const itemsController = {
-  getItem(ctx, next) {
-    const item = db.getItem(Number(ctx.params.itemId));
+  async getItem(ctx, next) {
+    const item = await ctx.app.db.getItem(ctx.params.itemId);
     ctx.body = item || { status: 'not found' };
     ctx.status = item ? 200 : 404;
   },
-  getItems(ctx, next) {
-    ctx.body = db.getItems();
+  async getItems(ctx, next) {
+    ctx.body = await ctx.app.db.getItems();
   },
-  createItem(ctx, next) {
-    if (ctx.headers.authorization === 'admin') {
-      ctx.body = db.writeItem(null, ctx.request.body);
-      ctx.status = 201;
-    } else {
+  async createItem(ctx, next) {
+    if (ctx.headers.authorization !== 'admin') {
       ctx.body = { status: 'you not allowed' };
       ctx.status = 401;
+      return;
     }
-  },
-  deleteItem(ctx, next) {
-    db.deleteItem(Number(ctx.params.itemId));
-    ctx.body = '';
-    ctx.status = 204;
-  },
-  updateItem(ctx, next) {
-    const validator = new LIVR.Validator({
-      itemId: 'positive_integer',
-    });
-    const itemId = Number(ctx.params.itemId);
+
     const itemData = ctx.request.body;
-
-    const validData = validator.validate({ itemId });
-
-    if (!validData) {
+    if (!dataValidation(itemData)) {
       ctx.body = validator.getErrors();
       ctx.status = 400;
       return;
     }
 
-    const updatedItem = db.updateItem(itemId, itemData);
+    ctx.body = await ctx.app.db.createItem(ctx.request.body);
+    ctx.status = 201;
+  },
+  async deleteItem(ctx, next) {
+    if (ctx.headers.authorization !== 'admin') {
+      ctx.body = { status: 'you not allowed' };
+      ctx.status = 401;
+      return;
+    }
+
+    await ctx.app.db.deleteItem(ctx.params.itemId);
+    ctx.body = '';
+    ctx.status = 204;
+  },
+  async updateItem(ctx, next) {
+    if (ctx.headers.authorization !== 'admin') {
+      ctx.body = { status: 'you not allowed' };
+      ctx.status = 401;
+      return;
+    }
+
+    const itemId = ctx.params.itemId;
+    const itemData = ctx.request.body;
+
+    if (!dataValidation(itemData)) {
+      ctx.body = validator.getErrors();
+      ctx.status = 400;
+      return;
+    }
+
+    const updatedItem = await ctx.app.db.updateItem(itemId, itemData);
 
     ctx.body = updatedItem || { status: 'not found' };
     ctx.status = updatedItem ? 204 : 404;
